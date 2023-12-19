f8a02d8b35a37cc337699bc3e56c0bf756d75983
mcarpent87 <mclean.carpenter@lendio.com>
2023-12-19 08:41:14 -0700

    add bs card component

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,20 @@
         <input
           type="text"
           class="form-control w-25"
-          placeholder="Search"
+          placeholder="Search..."
           aria-label="search"
         />
       </div>
-      <div class="container options-container"></div>
+      <div class="container options-container">
+        <div class="card" style="width: 18rem;">
+          <img src="https://t3.ftcdn.net/jpg/04/83/90/18/240_F_483901821_46VsNR67uJC3xIKQN4aaxR6GtAZhx9G8.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Math</h5>
+            <p class="card-text">15 Questions</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
