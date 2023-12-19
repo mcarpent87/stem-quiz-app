6633fdc5e7bd64237846856a5a22f5efa09480c2
McLean Carpenter <mcarpent@McLeans-MacBook-Pro.local>
2023-12-18 22:54:37 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="header my-3 d-flex">
+        <h1 class="fw-bold me-5">Quizzes</h1>
+        <input
+          type="text"
+          class="form-control w-25"
+          placeholder="Search"
+          aria-label="search"
+        />
+      </div>
+      <div class="container options-container"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
